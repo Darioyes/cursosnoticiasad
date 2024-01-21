8a4bda9bc99e481cd560eee0cfc82fb1789cb8a9
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2024-01-20 20:34:06 -0500

    archivosOrganizados

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    //declaramos la variable de la url de la api
+    apiUrlBase: 'http://127.0.0.1:8000/api/',
+    //declaramos el entorno producción
+    production: true
+};
