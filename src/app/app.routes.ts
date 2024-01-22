bf6197f1c9c12fcdb04ce98bd0a4537b6ee44d09
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2024-01-21 21:22:38 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./controllers/pages/login-page/login-page.component'),
+    },
+    {
+        path: 'home',
+        title: 'Home NotiWeb',
+        loadComponent: () => import('./controllers/pages/dashboard/dashboard.component'),
+    },
+    {
+        path:'**',
+        title: 'Not Found',
+        loadComponent: () => import('./controllers/pages/not-found-page/not-found-page.component'),
+    }
+
+];
