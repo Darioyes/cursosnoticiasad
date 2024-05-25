2049e00eda30c48d42080427ef4fd6cc72d84894
Dario Gutierrez Novoa <dario-yes@hotmail.com>
2024-05-24 19:42:02 -0500

    modificar app config para la ruta #

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -9,7 +9,10 @@ import { errorHandlerInterceptor } from './interceptor/error-handler.interceptor
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
+    provideRouter(routes,
+       //withHashLocation sirve para que en el servidor siempre vaya al index.html,
+    withHashLocation()
+    ),
     provideClientHydration(),
     // importProvidersFrom(
     //     //aca se guardan todos los modulos que se van a usar en la aplicacion
